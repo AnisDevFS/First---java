79c86bd46de0aa9836d24293d981dc5ae32eb75a
fr.anis.mouelhi <fr.anis.mouelhi@gmail.com>
2021-06-08 16:51:26 +0200

    premiere classe et test de variables

---
+++ src/com/first/test/Voiture.java
@@ -0,0 +1,26 @@
+package com.first.test;
+
+public class Voiture {
+
+	public static void main(String[] args) {
+		
+		// Exemple de commentaire
+		// declarer une variable c'est reserver une case mémoire
+		// pour stocker une valeur avec le meme type de la variable
+		// la taille de la case mémoire dépend du type de la variable
+		short sh;
+		long f;
+		float b = 7 , c = 4 ;
+		float k =  (float) (7 / 4);
+		/*
+		 * String email; 
+		 * email = "anis@pop.fr"; commentaire en block
+		 */
+		boolean p = false;
+		char m = 'm';
+		System.out.println("Héllo World ! " + ' ' + m + " " + c);
+		System.out.println( "m" + b + c);
+		System.out.println( k);
+
+	}
+}
