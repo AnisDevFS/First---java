74ac1e635ce712e4e645e2acebc74f077d7e167e
fr.anis.mouelhi <fr.anis.mouelhi@gmail.com>
2021-06-23 13:48:12 +0200

    prendre en considération tous les fichiers .class

---
+++ src/com/first/collections/TestPourGit.java
@@ -0,0 +1,11 @@
+package com.first.collections;
+
+public class TestPourGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test"); 
+	}
+
+}
